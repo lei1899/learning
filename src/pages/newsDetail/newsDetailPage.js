8ee4e4bca78ec6614bbe9f7536a1e37a64f4ca16
Lei Zhang <mrs.leizhang@gmail.com>
2025-02-13 13:17:29 -0500

    tweak css

--- src/pages/newsDetail/newsDetailPage.js
+++ src/pages/newsDetail/newsDetailPage.js
@@ -1,41 +1,48 @@
-import {useParams} from "react-router-dom";
-import {Container, TitleSection} from "./newsDetailPage.style";
-import {documentToReactComponents} from '@contentful/rich-text-react-renderer';
+import { useParams } from "react-router-dom";
+import { Container, TitleSection, StyledImage } from "./newsDetailPage.style";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import FetchNewsDetailEntry from "../../api/newsDetail";
-import {useEffect, useState} from 'react';
-import {RichtextContent} from "../../components/common/richtextContent/richtextContent.style";
+import { useEffect, useState } from "react";
+import { RichtextContent } from "../../components/common/richtextContent/richtextContent.style";
 import Footer from "../../components/common/footer/footer";
 import getNestedObjectValue from "../../common_check/getValue";
 
 function NewsDetailPage() {
-    let {newsId} = useParams();
+  let { newsId } = useParams();
 
-    const [newsDetailData, setNewsDetailData] = useState(null);
+  const [newsDetailData, setNewsDetailData] = useState(null);
 
-    useEffect(() => {
-        FetchNewsDetailEntry(newsId).then(data => setNewsDetailData(data));
-    }, [newsId]);
+  useEffect(() => {
+    FetchNewsDetailEntry(newsId).then((data) => setNewsDetailData(data));
+  }, [newsId]);
 
-    if (!newsDetailData) {
-        return <></>;
-    }
-    console.log("==detail=", newsDetailData);
-    return (
-        <Container>
-            <TitleSection>
-                <h1>{newsDetailData.title}</h1>
-            </TitleSection>
-            <div>
-                <img alt="" width={400} src={getNestedObjectValue(newsDetailData, 'image.fields.file.url')}/>
-            </div>
-            <div>
-                <audio src={getNestedObjectValue(newsDetailData, 'audio.fields.file.url')} controls></audio>
-            </div>
-            <RichtextContent>{documentToReactComponents(newsDetailData.keywords)}</RichtextContent>
-            <Footer/>
-        </Container>
-    );
-
+  if (!newsDetailData) {
+    return <></>;
+  }
+  console.log("==detail=", newsDetailData);
+  return (
+    <Container>
+      <TitleSection>
+        <h1>{newsDetailData.title}</h1>
+      </TitleSection>
+      <div>
+        <StyledImage
+            alt=""
+            src={getNestedObjectValue(newsDetailData, 'image.fields.file.url')}
+        />
+      </div>
+      <div>
+        <audio
+          src={getNestedObjectValue(newsDetailData, "audio.fields.file.url")}
+          controls
+        ></audio>
+      </div>
+      <RichtextContent>
+        {documentToReactComponents(newsDetailData.keywords)}
+      </RichtextContent>
+      <Footer />
+    </Container>
+  );
 }
 
 export default NewsDetailPage;
--- src/pages/newsDetail/newsDetailPage.style.js
+++ src/pages/newsDetail/newsDetailPage.style.js
@@ -3,27 +3,35 @@ import QuoteCard from "../../components/common/quoteCard/quoteCard.style";
 import { PageContainer } from "../../components/common/pageContainer/pageContainer.style";
 
 export const Container = styled(PageContainer)`
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center; // 让子元素在交叉轴（垂直方向）上居中
+  justify-content: center; // 让子元素在主轴（水平方向）上居中
+`;
 
 export const TitleSection = styled(QuoteCard)`
-    display:flex;
-    flex-direction:column;
-    align-items: stretch;
-    margin-bottom:100px;
-    @media screen and (max-width:769px) {
-        margin-bottom:0;
-    }
-    h1 {
-        margin-bottom:1rem;
-    }
-    h4 {
-        padding-left:1rem;
-    }
-    img {
-        transform: translateY(100px);
-        object-fit: cover;
-        max-width: 90%;
-        max-height: 400px;
-        overflow:hidden;
-    }
-`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  margin-bottom: 100px;
+  @media screen and (max-width: 769px) {
+    margin-bottom: 0;
+  }
+  h1 {
+    margin-bottom: 1rem;
+  }
+  h4 {
+    padding-left: 1rem;
+  }
+  img {
+    transform: translateY(100px);
+    object-fit: cover;
+    max-width: 90%;
+    max-height: 400px;
+    overflow: hidden;
+  }
+`;
+
+export const StyledImage = styled.img`
+    width: 400px;
+`;
